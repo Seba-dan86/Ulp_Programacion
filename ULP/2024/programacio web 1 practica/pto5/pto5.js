2207319395c901a5897983de60c86e6191f22afc
Sebastian D'Andrea <sevasdandrea86@gmail.com>
2024-03-18 20:31:18 -0300

    actualizacion web 1

---
+++ ULP/2024/programacio web 1 practica/pto5/pto5.js
@@ -0,0 +1,19 @@
+const btnTarjetas= document.getElementById('btnTarjetas');
+const btnTable= document.getElementById('btnTable');
+
+
+function btnTablas(){
+    alert("Ud dio click en el boton ")
+
+};
+
+btnTarjetas.addEventListener('click',btnTablas());
+btnTable.addEventListener('click',btnTablas());
+
+
+// const img= document.getElementById('img');
+
+// img.addEventListener('onMouseover',function() {
+
+//     img.className.toggle('hide');
+// });
